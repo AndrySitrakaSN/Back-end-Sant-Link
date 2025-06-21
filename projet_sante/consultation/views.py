50612b59fdd21bc21c1b6c3ddea9558c4375e5e1
Aetius <randrianasolotoutous@gmail.com>
2025-06-21 17:51:07 +0300

    consultation views update

--- projet_sante/consultation/views.py
+++ projet_sante/consultation/views.py
@@ -19,37 +19,37 @@ class ConsultationRetrieveUpdateDeleteView(generics.RetrieveUpdateDestroyAPIView
 
 @api_view(['GET'])
 def liste_consultations(request):
-    consultations = Consultation.objects.all()
-    serializer = ConsultationSerializer(consultations, many=True)
-    return Response(serializer.data)
+    consultations_liste = Consultation.objects.all()
+    serializer_liste = ConsultationSerializer(consultations_liste, many=True)
+    return Response(serializer_liste.data)
 
 @api_view(['POST'])
 def ajouter_consultation(request):
-    serializer = ConsultationSerializer(data=request.data)
-    if serializer.is_valid():
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
-    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    serializer_ajout = ConsultationSerializer(data=request.data)
+    if serializer_ajout.is_valid():
+        serializer_ajout.save()
+        return Response(serializer_ajout.data, status=status.HTTP_201_CREATED)
+    return Response(serializer_ajout.errors, status=status.HTTP_400_BAD_REQUEST)
 
 @api_view(['PUT', 'PATCH'])
 def modifier_consultation(request, pk):
     try:
-        consultation = Consultation.objects.get(pk=pk)
+        consultation_update = Consultation.objects.get(pk=pk)
     except Consultation.DoesNotExist:
         return Response({'error': 'Consultation non trouvée'}, status=status.HTTP_404_NOT_FOUND)
     
-    serializer = ConsultationSerializer(consultation, data=request.data, partial=True)
-    if serializer.is_valid():
-        serializer.save()
-        return Response(serializer.data)
-    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    serializer_update = ConsultationSerializer(consultation_update, data=request.data, partial=True)
+    if serializer_update.is_valid():
+        serializer_update.save()
+        return Response(serializer_update.data)
+    return Response(serializer_update.errors, status=status.HTTP_400_BAD_REQUEST)
 
 @api_view(['DELETE'])
 def supprimer_consultation(request, pk):
     try:
-        consultation = Consultation.objects.get(pk=pk)
+        consultation_delete = Consultation.objects.get(pk=pk)
     except Consultation.DoesNotExist:
         return Response({'error': 'Consultation non trouvée'}, status=status.HTTP_404_NOT_FOUND)
 
-    consultation.delete()
+    consultation_delete.delete()
     return Response({'message': 'Consultation supprimée avec succès'}, status=status.HTTP_204_NO_CONTENT)
